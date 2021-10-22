fa22d8c937756b62cdd11f2602a43e3a078ce615
CondimentCommander <64157688+CondimentCommander@users.noreply.github.com>
2021-10-22 08:31:47 -0400

    Plot growth stuff

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var Game = {
 		let tile = Plot.tiles[ev.srcElement.parentElement.rowIndex][ev.srcElement.cellIndex]
 		let id = tile.sprite;
 		let sprite = Graphics.elems[id];
-		if (sprite.img == 'images/grassbad.png') Graphics.elems[id].replace(new Graphics.AnimatedSpriteElement(tile.x, tile.y, { img: 'images/animtest.png', count: 8, rc: 4, ft: 5, s: 12, viewLayer: 3 }));
-		if (sprite.img == 'images/animtest.png') Graphics.elems[id].replace(new Graphics.SpriteElement(tile.x, tile.y, { img: 'images/grassbad.png', s: 16, opacity: 0.5, viewLayer: 3 }));
+		if (sprite.img == 'images/grassbad.png') Graphics.elems[id].replace(new Graphics.AnimatedSpriteElement(tile.x, tile.y, { img: 'images/animtest.png', count: 8, rc: 4, ft: 5, s: 12, viewLayer: 2 }));
+		if (sprite.img == 'images/animtest.png') Graphics.elems[id].replace(new Graphics.SpriteElement(tile.x, tile.y, { img: 'images/grassbad.png', s: 16, opacity: 0.5, viewLayer: 2 }));
 	},
 	mouseDown: false,
 	panStartX: 0,
@@ -69,7 +69,7 @@ var Game = {
 	},
 	init: () => {
 		Game.plants = [
-			new Game.Plant('test', 0, 'Test').setGrowth({speed: 2, matureTime: 5, decay: 1, stages: ['images/grassbad.png']}).setinh()
+			new Game.Plant('test', 0, 'Test').setGrowth({speed: 2, matureTime: 5, decay: 1, stages: [{ img: 'images/animtest.png', count: 8, rc: 4, ft: 5, s: 12, viewLayer: 3, anim: true }, { img: 'images/grassbad.png', s: 16, opacity: 0.5, viewLayer: 3, anim: false }]}).setinh()
 		];
 	}
 };
@@ -99,7 +99,7 @@ var Plot = {
 			this.plotx = x;
 			this.ploty = y;
 			this.soil = soil;
-			this.plant = null;
+			this.plant = undefined;
 		}
 	},
 	PlantTile: class {
@@ -110,7 +110,9 @@ var Plot = {
 			this.stage = 0;
 			this.life = 0;
 			this.stagetime = 0;
-			this.sprite = new Graphics.AnimatedSpriteElement(this.tile.x, this.tile.y, { img: this.inh.growth.stages[0], s: 16, opacity: 1.0, viewLayer: 3 }).add();
+			this.grows = 0;
+			if (this.inh.growth.stages[0].anim) this.sprite = new Graphics.AnimatedSpriteElement(this.tile.x, this.tile.y, this.inh.growth.stages[0]).add();
+			if (!this.inh.growth.stages[0].anim) this.sprite = new Graphics.SpriteElement(this.tile.x, this.tile.y, this.inh.growth.stages[0]).add();
 		}
 	},
 	execute: (func) => {
@@ -122,9 +124,55 @@ var Plot = {
 	},
 	tick: () => {
 		Plot.execute((tile, i, j) => {tile.plant.inh.events.pretick()});
-		Plot.execute((tile, i, j) => {
-			
-		});
+		Plot.execute((tile, i, j) => {Plot.grow(tile)});
+		console.log('tick!');
+	},
+	stop: () => {
+		clearInterval(Plot.cycle);
+	},
+	grow: (tile) => {
+		let plant = tile.plant;
+		plant.life++;
+		if (plant.inh.growth.speed == 0) return;
+		let length = plant.inh.growth.stages.length;
+		if (plant.stage == 0) {
+			plant.stagetime++;
+			let chance = plant.inh.growth.speed * 0.04;
+			let dbchance = plant.inh.growth.speed * 0.01
+			let rand = Math.random();
+			if (rand <= chance) {
+				console.log('grew');
+				if (rand <= dbchance) plant.grows += 2;
+				if (rand > dbchance) plant.grows++;
+				if (plant.grows >= length) {
+					plant.stage = 1;
+					plant.stagetime = 0;
+					plant.grows = 0;
+					Plot.mutate(tile);
+				}
+			}
+		} else {
+			if (plant.stage == 1) {
+				plant.stagetime++;
+				if (plant.stagetime >= plant.inh.growth.matureTime) {
+					plant.stage = 2;
+					plant.stagetime = 0;
+				}
+			} else {
+				if (plant.stage == 2) {
+					plant.stagetime++;
+					let chance = plant.inh.growth.speed * 0.01 * plant.inh.decay * Math.pow(plant.stagetime, 2);
+					let rand = Math.random();
+					if (rand <= chance) Plot.decay(tile);
+				}
+			}
+		}
+	},
+	mutate: (tile) => {
+		
+	},
+	decay: (tile) => {
+		console.log('goodbye');
 	},
 	render: () => {
 		let ps = Graphics.screenInfo().ps;
@@ -150,6 +198,13 @@ var Plot = {
 				sprite.prop();
 			}
 		}
+		for (let i = 0; i < 11; i++) {
+			Object.values(Graphics.elemLayers[i]).forEach((element) => {
+				if (element.pan) {
+					
+				}
+			});
+		}
 	},
 	renderUpdate: () => {
 		for (let i = 0; i < Plot.height; i++) {
@@ -163,136 +218,7 @@ var Plot = {
 const Language = {};
 var Data = {};
 
-var Graphics = {
-	ScreenElement: class {
-		constructor(x, y, data = { viewLayer: 5, opacity: 1.0 }) {
-			this.pos = {x: x, y: y};
-			this.lr = data.viewLayer;
-			if (this.lr == undefined) this.lr = 5;
-			this.op = data.opacity;
-			this.data = data;
-			this.id = this.randomId();
-			this.canvas = this.renderInfo();
-		}
-		randomId() {
-			let rand = Math.floor(Math.random() * 10000);
-			while (Object.keys(Graphics.elems).includes(rand.toString())) {
-				rand = Math.floor(Math.random() * 10000);
-			}
-			return rand;
-		}
-		add() {
-			Graphics.elems[this.id] = this;
-			Graphics.elemLayers[this.lr][this.id] = this;
-			return this.id;
-		}
-		remove() {
-			delete Graphics.elemLayers[this.lr][this.id];
-			delete Graphics.elems[this.id];
-		}
-		renderInfo() {
-			if (this.lr > 5) return Graphics.ov;
-			if (this.lr <= 5) return Graphics.ctx;
-		}
-		predraw() { //for general actions done before drawing an element
-			Graphics.ctx.globalAlpha = this.opacity; //used to set opacity of the element
-		}
-		prop() { //updates the current value of the element in the element list to be in the layer
-			Graphics.elemLayers[this.lr][this.id] = this;
-		}
-		replace(rp) {
-			rp.id = this.id;
-			Graphics.elemLayers[this.lr][this.id] = rp;
-			//Graphics.elemLayers[this.lr][this.id].id = this.id;
-			Graphics.elems[this.id] = rp;
-		}
-	},
-	defineElements: () => {
-		Graphics.SpriteElement = class extends Graphics.ScreenElement {
-			constructor(x = 0, y = 0, data = { img: '', s: 1, viewLayer: 5, opacity: 1.0 }) {
-				super(x, y, data);
-				this.img = data.img; //static image to use for the sprite
-				this.image = new Image(); //image object loaded for the sprite on creation
-				this.ready = false;
-				this.image.onload = () => {
-					this.ready = true;
-				};
-				this.image.src = this.img;
-				this.scale = data.s;
-			}
-			draw() {
-				this.predraw();
-				this.canvas.drawImage(this.image, this.pos.x, this.pos.y, this.scale, this.scale);
-			}
-		}
-		Graphics.AnimatedSpriteElement = class extends Graphics.SpriteElement {
-			constructor(x = 0, y = 0, data = { img: '', ft: 5, count: 1, rc: 1, s: 1, viewLayer: 5, opacity: 1.0 }) {
-				super(x, y, data);
-				this.frametime = data.ft; //amount of frames to hold each frame image for
-				this.count = data.count; //total number of frames in the spritesheet
-				this.rc = data.rc; //anmount of frames per row
-				this.frame = 0; //the amount of frames since the last cycle. Does not represent the frametime
-				this.calcframe = 0; //the frame of the image to use, that respects the frame hold time
-			}
-			draw() {
-				this.predraw();
-				this.calcframe = Math.ceil((this.frame + 1) / this.frametime) - 1;
-				let size = this.image.width / this.rc;
-				let calcrow = Math.floor(this.calcframe / this.rc) * size;
-				let calcpos = (this.calcframe % this.rc) * size;
-				this.canvas.drawImage(this.image, calcpos, calcrow, size, size, this.pos.x, this.pos.y, size, size);
-				this.frame++;
-				if (this.frame >= (this.count) * this.frametime) this.frame = 0;
-			}
-		}
-	},
-	setPos: (elem, x, y) => {
-		let rect = elem.getBoundingClientRect();
-		elem.style.left = x + 'px';
-		elem.style.top = y + 'px';
-	},
-	screenInfo: () => {
-		let pixelsize = Plot.zoom / 2;
-		let screensize = Plot.farm.clientWidth;
-		let tilesize = screensize / Plot.zoom;
-		return {ts: tilesize, ps: pixelsize, ss: screensize};
-	},
-	refresh: () => {
-		Graphics.ctx.clearRect(0, 0, 512, 512);
-		Graphics.ov.clearRect(0, 0, 1280, 720);
-	},
-	elems: {},
-	elemLayers: [],
-	testPattern: () => {
-		for (let i = 0; i < 512; i+=16) {
-			for (let j = 0; j < 512; j+=16) {
-				if (i % 32 == 0 && j % 32 == 0) {
-					Graphics.ctx.fillRect(i, j, 16, 16);
-				}
-			}
-		}
-	},
-	initial: () => {
-		Graphics.defineElements();
-		for (let i = 0; i < 11; i++) {
-			Graphics.elemLayers.push({});
-		}
-		//Graphics.foo = new Graphics.AnimatedSpriteElement(0, 0, { img: 'images/animtest.png', count: 8, rc: 4, ft: 5, s: 12, viewLayer: 0 }).add();
-		//Graphics.bar = new Graphics.SpriteElement(100, 100, { img: 'images/grassbad.png', s: 16, opacity: 0.5, viewLayer: 0 }).add();
-		Graphics.interval = setInterval(Graphics.update, 1000 / 30);
-	},
-	update: () => {
-		Graphics.refresh();
-		for (let i = 0; i < 11; i++) {
-			Object.values(Graphics.elemLayers[i]).forEach((element) => {
-				element.draw();
-			});
-		}
-	},
-	stop: () => {
-		clearInterval(Graphics.interval);
-	},
-};
+
 function start() {
 	Plot.farm = document.getElementById('farmview');
 	Plot.plot = document.getElementById('plot');
@@ -305,6 +231,7 @@ function start() {
 	Graphics.initial();
 	Plot.generate();
 	Plot.render();
+	Plot.cycle = setInterval(Plot.tick, 500);
 	console.log('Loaded!');
 }
 
@@ -313,3 +240,5 @@ function start() {
 // also break it up into multiple files maybe?     graphics on separate, plot on separate
 //add spriteelement option for 'panned', which means that there is no extra code needed and the sprite will move with the plot
 //make stages include sprite data (not just name)
+//decay stuff
+//yeah boi
